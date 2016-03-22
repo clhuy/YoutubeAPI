f8b38bd943b3af35c6ee58e18e4337d86c6429e5
Cu Le Huy <lcu@usc.edu>
2016-03-22 02:29:30 -0700

    Add video player

--- bootstrap/js/search.js
+++ bootstrap/js/search.js
@@ -1,15 +1,21 @@
 const MAX_RESULTS = 12;
 
-function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+//function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
 
 function init() {
 	gapi.client.setApiKey('AIzaSyCKV_Pj62t5ePv1D_hx70dmZC0k_dmDGVc');
 	gapi.client.load('youtube', 'v3', function(){});
 }
 
+function addClick(i,s) { 
+	$('#img'+i).on('click', function(){
+		var source = '//www.youtube.com/embed/'+s+'/0.jpg/?autoplay=1';
+		$('#frame').attr('src', source);
+	});
+}
+
 function search(){
 	var q = encodeURIComponent($("#search-bar").val()).replace(/%20/g, "+");
-	console.log(q);
     var request = gapi.client.youtube.search.list({
         part: "snippet",
         type: "video",
@@ -22,22 +28,21 @@ function search(){
 		//$("#videos").html("");
 		var results = response.result;
 		$.each(results.items, function(index, item){
-			var html = "<div class='col-lg-3 col-md-4 col-xs-6'>" + 
-							"<a class='thumbnail' href='#'>" +
-								"<img class='img-responsive' src='http://img.youtube.com/vi/" + item.id.videoId + "/0.jpg' alt=''>" +
-							"</a>" +
+			var id = "img" + index;
+			var html = "<div class='col-lg-4 col-md-4 col-xs-6 thumb'>" + 
+							//"<a class='thumbnail' href='#'>" +
+								"<img class='img-responsive' id='" + id + "' src='http://img.youtube.com/vi/" + item.id.videoId + "/0.jpg' alt=''>" +
+							//"</a>" +
 							//"<div class='title'>"+item.snippet.title+"</div>"+
 						"</div>";
-
 			$("#results").append(html);
+			addClick(index, item.id.videoId);
 			/*$.get("video_item/item.html", function(data){
 				$("#results").append(tplawesome(data, [{"videoid":item.id.videoId}]));
 			});*/
 		});
 	});
 }
-
-
 
 /*function ontClientLoad(){
 	gapi.client.load("youtube", "v3", onYoutubeApiLoad);
